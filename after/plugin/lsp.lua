c5eb0f49b93e64929e27d08ffda11977cc73ba8b
JakubSzukala <burski09@outlook.com>
2023-12-15 15:05:02 +0100

    Add TAB autocomplete

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,17 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+-- TAB Autocomplete
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp_action.tab_complete(),
+    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+  })
+})
+
 -- LSP Servers manager
 require('mason').setup({})
 require('mason-lspconfig').setup({
