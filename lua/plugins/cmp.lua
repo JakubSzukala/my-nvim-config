d4d046ddfca88ece6c52995d86a7c348c3de0425
JakubSzukala <burski09@outlook.com>
2024-10-13 23:23:59 +0200

    Working supertab

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,16 +1,46 @@
---return {
---  "hrsh7th/nvim-cmp",
---  version = false, -- Last release is way too old
---  event = "InsertEnter",
---  dependencies = {
---    "hrsh7th/cmp-nvim-lsp",
---    "hrsh7th/cmp-buffer",
---    "hrsh7th/cmp-path",
---  },
---  opts = function()
---    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
---    local cmp = require("cmp")
---    local defaults = require("cmp.config.default")()
---    local auto_select = true
---  end
---}
+return {
+  "hrsh7th/nvim-cmp",
+  version = false, -- Last release is way too old
+  event = "InsertEnter",
+  dependencies = {
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+  },
+  opts = function()
+    -- Ghost text will look the same way comments do, thanks to linking to Comment group
+    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
+    local cmp = require("cmp")
+    local defaults = require("cmp.config.default")()
+    return {
+      -- Leaving this as a note, noinsert prevents insertion of first item from the list
+      --completion = {
+        --completeopt = "menu,menuone,noinsert",
+      --},
+
+      preselect = cmp.PreselectMode.Item,
+      mapping = cmp.mapping.preset.insert({
+        ['<Tab>'] = cmp.mapping(function(_)
+	  cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+        end, { "i", "c" }),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+      }),
+      -- Completion sources - https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "path" },
+      }, {
+        { name = "buffer" },
+      }),
+      experimental = {
+        ghost_text = {
+          hl_group = "CmpGhostText",
+        },
+      },
+      sorting = defaults.sorting
+    }
+  end
+}
