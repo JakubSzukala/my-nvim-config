e6d127dbbfe2323e378df74ba826f3b3cc841191
JakubSzukala <burski09@outlook.com>
2024-10-14 08:19:11 +0200

    Add maps and sets

--- lua/js/remap.lua
+++ lua/js/remap.lua
@@ -7,3 +7,18 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Telescope remaps
 vim.keymap.set("n", "<leader>pf", require("telescope.builtin").find_files)
 vim.keymap.set("n", "<leader>ps", function() require("telescope.builtin").grep_string({search = vim.fn.input("Grep > ") }) end)
+
+-- Grab and shift text in visual mode (doesn't work at the bottom of the file)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Allow jumping over folded line
+-- TODO: Do this only for certain filetypes like markdown where line wrapping is nice
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+
+-- Keep the cursor in one place while half page jumping and searching
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
---
+++ lua/js/set.lua
@@ -0,0 +1,37 @@
+-- Set leader character
+vim.g.mapleader = " "
+
+-- Cursor settings
+vim.opt.guicursor = "i-ci:ver30-iCursor-blinkwait300-blinkon200-blinkoff150"
+
+-- Line settings
+vim.opt.nu = true
+vim.opt.relativenumber = false
+
+-- Indentation settings
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Line wrapping
+vim.opt.wrap = false
+
+-- File safety settings
+vim.opt.swapfile = true
+vim.opt.backup = false
+
+-- Search settings
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Looks
+vim.opt.termguicolors = true
+vim.opt.colorcolumn = "80"
+
+-- Misc
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
