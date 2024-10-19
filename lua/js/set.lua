0a4ebbbf563aac7f5cdc16125608c437ec559a36
JakubSzukala <burski09@outlook.com>
2024-10-19 09:03:32 +0200

    Add autocommands changing formatting for different programming languages

--- lua/js/set.lua
+++ lua/js/set.lua
@@ -1,3 +1,8 @@
+-- Below is set of default options. In the next section there is set of
+-- auto commands that override these based on file type. For example, I like
+-- C programms to expand tab to 8 spaces, but the same setting messes up
+-- gdscript and probably Python.
+
 -- Set leader character
 vim.g.mapleader = " "
 
@@ -8,11 +13,11 @@ vim.opt.guicursor = "i-ci:ver30-iCursor-blinkwait300-blinkon200-blinkoff150"
 vim.opt.nu = true
 vim.opt.relativenumber = false
 
--- Indentation settings
+-- Indentation settings - see :h tabstop 1
 vim.opt.tabstop = 8
 vim.opt.softtabstop = 8
 vim.opt.shiftwidth = 8
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.opt.smartindent = true
 
 -- Line wrapping
@@ -38,3 +43,23 @@ vim.opt.list = true
 vim.opt.scrolloff = 8
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
+
+-- File type dependent overrides
+
+-- C/C++ files
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.c", "*.h", "*.cpp", "*.hpp" },
+  callback = function(_)
+    vim.opt.softtabstop = 8
+    vim.opt.shiftwidth = 8
+  end
+})
+
+-- Lua files
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.lua" },
+  callback = function(_)
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+  end
+})
