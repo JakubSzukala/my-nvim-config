1cd864f0b5ed921c6c8d3bd5df1a7d38d3f5deff
JakubSzukala <burski09@outlook.com>
2026-04-09 21:20:06 +0200

    Make some settings more verbose for learning, add closing of brackets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -22,10 +22,12 @@ return {
 			preselect = cmp.PreselectMode.Item,
 			mapping = cmp.mapping.preset.insert({
 				["<Tab>"] = cmp.mapping(function(fallback)
-					local col = vim.fn.col(".") - 1
+					local cursor_col = vim.fn.col(".") - 1
+					local cursor_char = vim.fn.getline("."):sub(cursor_col, cursor_col)
+					local cursor_char_is_space = cursor_char:match("%s") ~= nil
 					if cmp.visible() then
 						cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-					elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+					elseif cursor_col == 0 or cursor_char_is_space then
 						fallback()
 					else
 						cmp.complete()
@@ -34,7 +36,17 @@ return {
 				["<S-Tab>"] = cmp.mapping.select_prev_item(),
 				["<C-u>"] = cmp.mapping.scroll_docs(-4),
 				["<C-d>"] = cmp.mapping.scroll_docs(4),
+				["("] = cmp.mapping(function(fallback)
+					vim.api.nvim_put({"()"}, "c", true, false)
+				end, { "i" }),
+				["{"] = cmp.mapping(function(fallback)
+					vim.api.nvim_put({"{}"}, "c", true, false)
+				end, { "i" }),
+				["["] = cmp.mapping(function(fallback)
+					vim.api.nvim_put({"[]"}, "c", true, false)
+				end, { "i" }),
 			}),
+
 			-- Completion sources - https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
